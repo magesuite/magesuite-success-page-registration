9f8aded1b9c5ae93d8d86c385f6a344a0533cd5f
Paweł Złamaniec <pawel.zlamaniec@creativestyle.pl>
2020-08-28 11:04:35 +0200

    Added check if client with the same email address exists and and displaying register form accordingly

---
+++ Block/Customer/Form/Register.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace MageSuite\SuccessPageRegistration\Block\Customer\Form;
+
+class Register extends \Magento\Customer\Block\Form\Register
+{
+    /**
+     * @var \Magento\Checkout\Model\Session
+     */
+    protected $checkoutSession;
+
+    /**
+     * @var \Magento\Customer\Model\CustomerFactory
+     */
+    protected $customerModelFactory;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Directory\Helper\Data $directoryHelper,
+        \Magento\Framework\Json\EncoderInterface $jsonEncoder,
+        \Magento\Framework\App\Cache\Type\Config $configCacheType,
+        \Magento\Directory\Model\ResourceModel\Region\CollectionFactory $regionCollectionFactory,
+        \Magento\Directory\Model\ResourceModel\Country\CollectionFactory $countryCollectionFactory,
+        \Magento\Framework\Module\Manager $moduleManager,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Customer\Model\Url $customerUrl,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Customer\Model\CustomerFactory $customerModelFactory,
+        array $data = [],
+        \Magento\Newsletter\Model\Config $newsLetterConfig = null
+
+    ) {
+        $this->checkoutSession = $checkoutSession;
+        $this->customerModelFactory = $customerModelFactory;
+
+        parent::__construct(
+            $context,
+            $directoryHelper,
+            $jsonEncoder,
+            $configCacheType,
+            $regionCollectionFactory,
+            $countryCollectionFactory,
+            $moduleManager,
+            $customerSession,
+            $customerUrl,
+            $data,
+            $newsLetterConfig
+        );
+    }
+
+    protected function _toHtml()
+    {
+        if(!$this->_customerSession->isLoggedIn()) {
+            $email = $this->checkoutSession->getLastRealOrder()->getCustomerEmail();
+            $websiteId = $this->_storeManager->getStore()->getWebsiteId();
+
+            $customer = $this->customerModelFactory->create();
+            $customer->setWebsiteId($websiteId)->loadByEmail($email);
+            if ($customer->getId()) {
+                return '';
+            }
+        }
+
+        return parent::_toHtml();
+    }
+}
