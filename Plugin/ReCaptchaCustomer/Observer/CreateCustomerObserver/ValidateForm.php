3525803ea3671527fda59ae311824e141528f06a
Blazej Doleska <blazej.doleska@creativestyle.pl>
2021-07-06 09:15:18 +0200

    added logic to work also with invisible recaptcha

---
+++ Plugin/ReCaptchaCustomer/Observer/CreateCustomerObserver/ValidateForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MageSuite\SuccessPageRegistration\Plugin\ReCaptchaCustomer\Observer\CreateCustomerObserver;
+
+class ValidateForm
+{
+    /**
+     * @var \Magento\Framework\App\Response\RedirectInterface
+     */
+    protected $redirect;
+
+    public function __construct(
+        \Magento\Framework\App\Response\RedirectInterface $redirect
+    ) {
+        $this->redirect = $redirect;
+    }
+
+    public function aroundExecute(\Magento\ReCaptchaCustomer\Observer\CreateCustomerObserver $subject, callable $proceed, \Magento\Framework\Event\Observer $observer)
+    {
+        if (strpos($this->redirect->getRefererUrl(), 'checkout/onepage/success') !== false) {
+            return $subject;
+        }
+
+        return $proceed($observer);
+    }
+}
