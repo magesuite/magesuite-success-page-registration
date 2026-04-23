54c2e8b97c813b5f89c92c01a831eff27125d447
Piotr Matras <piotr.matras@creativestyle.pl>
2026-04-23 09:17:30 +0200

    feat: MGS-5699 remove order id from session

---
+++ Observer/UnsetSalesAssignOrderId.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageSuite\SuccessPageRegistration\Observer;
+
+class UnsetSalesAssignOrderId implements \Magento\Framework\Event\ObserverInterface
+{
+    public function __construct(
+        protected \Magento\Customer\Model\Session $customerSession
+    ) {}
+
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $delegatedNewCustomerData = $this->customerSession->getDelegatedNewCustomerData();
+
+        if (isset($delegatedNewCustomerData['delegated_data']['__sales_assign_order_id'])) {
+            unset($delegatedNewCustomerData['delegated_data']['__sales_assign_order_id']);
+        }
+
+        $this->customerSession->setDelegatedNewCustomerData($delegatedNewCustomerData);
+    }
+}
